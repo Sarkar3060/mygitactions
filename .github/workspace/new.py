fd2187ebadb3c440f1eab3838463d27a8e2cfe71
Shantanu Shinde <shantanu.kshinde@gmail.com>
2025-02-03 10:35:36 +0530

    Update new.py

--- .github/workspace/new.py
+++ .github/workspace/new.py
@@ -1,9 +1,8 @@
+# example.py
+
 def my_function():
     x = 10
-    y = 20
-
-    
-    print(x + y)  
-
+    y = 20  # Corrected: y should be an integer
+    print(x + y)  # Now this will work as both are integers
 
 my_function()
